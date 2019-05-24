e5bc1f9298b6c2b35964279b4eac628bed8ca82f
Michael Fedell <michaelfedell14@gmail.com>
2019-05-23 21:17:49 -0500

    Update styling for Bootstrap4

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,8 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/game')
+def game():
+    return render_template('game.html', genres=[{"name": "jazz", "id": 0}, {"name": "pop", "id": 1}])
